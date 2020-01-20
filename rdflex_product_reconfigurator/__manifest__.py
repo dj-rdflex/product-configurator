4e7ad70b01fff8c99edd55e48821e5b7ed55fba3
DJ Patel <dj@rdflex.com>
2020-01-20 20:02:24 +0530

    Added a 'rdflex_product_reconfigurator' module.

---
+++ rdflex_product_reconfigurator/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Product Reconfigurator",
+    'summary': """
+        This module allows you to reconfigure a product in sales order
+    """,
+    'description': """
+        This module allows you to reconfigure a product in sales order
+    """,
+    'author': "RDFlex",
+    'website': "https://rdflex.com",
+    'category': 'Sales',
+    'version': '0.1',
+    'depends': ['sale_management'],
+    'images': ['static/description/banner.jpg'],
+    'data': [
+        'views/configurate_assets.xml',
+        'views/product_configurator_views.xml',
+        'views/templates.xml',
+    ],
+}
